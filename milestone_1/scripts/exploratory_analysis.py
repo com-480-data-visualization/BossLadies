bf9b058899cfe600dc360cce9afaaac2f3fe4210
Anna Tuz <annatuz11112005@gmail.com>
2026-03-20 10:00:57 +0100

    Clean up

--- milestone_1/scripts/exploratory_analysis.py
+++ milestone_1/scripts/exploratory_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-def load_food_data():
+def load_data():
     project_root = Path(__file__).resolve().parent.parent.parent
     data_dir = project_root / "data"
     
@@ -20,24 +20,3 @@ def save_plot(name, dpi=300):
     figures_path = create_figures_folder()
     file_path = figures_path / f"{name}.png"
     plt.savefig(file_path, dpi=dpi, bbox_inches="tight")
-
-'''
-COUNTRY_NAME_MAP = {
-    "Türkiye": "Turkiye",
-    "Korea": "South Korea",
-    "Slovak Republic": "Slovakia"
-}
-
-def load_data():
-    project_root = Path(__file__).resolve().parent.parent.parent
-    data_dir = project_root / "data"
-    
-    bli = pd.read_csv(data_dir / "better-life-index-2024.csv")
-    whr = pd.read_csv(data_dir / "WHR2024.csv")
-    return bli, whr
-
-def normalize_country(val):
-    if pd.isna(val): return val
-    name = str(val).strip()
-    return COUNTRY_NAME_MAP.get(name, name)
-'''
